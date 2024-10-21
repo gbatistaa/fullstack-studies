b1bc260e8ac5ec739a783d9230f2c1d9c987e837
gbatistaa <gabrielbatistabarbosa4@gmail.com>
2024-10-21 20:56:32 -0300

    eslint configuration done

--- React/learning-jotai/eslint.config.js
+++ React/learning-jotai/eslint.config.js
@@ -1,28 +1,64 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
 
-export default tseslint.config(
-  { ignores: ['dist'] },
+export default [
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
+    ignores: ["node_modules"],
+  },
+  {
+    files: ["**/*.ts", "**/*.tsx"],
     plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+      "@typescript-eslint": require("@typescript-eslint/eslint-plugin"),
+      react: require("eslint-plugin-react"),
+      prettier: require("eslint-plugin-prettier"),
+      import: require("eslint-plugin-import"),
+      "react-hooks": require("eslint-plugin-react-hooks"),
+    },
+    languageOptions: {
+      parser: require("@typescript-eslint/parser"),
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
     },
     rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
+      "no-use-before-define": "off",
+      "@typescript-eslint/no-use-before-define": ["error"],
+      "react/jsx-filename-extension": ["warn", { extensions: [".tsx"] }],
+      "import/extensions": "off", // Desabilite essa regra
+      "no-shadow": "off",
+      "@typescript-eslint/no-shadow": ["error"],
+      "@typescript-eslint/explicit-function-return-type": [
+        "error",
+        {
+          allowExpressions: true,
+        },
       ],
+      "max-len": [
+        "warn",
+        {
+          code: 100,
+          ignoreComments: true,
+          ignoreUrls: true,
+        },
+      ],
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
+      "import/prefer-default-export": "off",
+      "react/prop-types": "off",
+      "prettier/prettier": ["error", { endOfLine: "auto", singleQuote: false }],
+      quotes: ["error", "double"],
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+      "import/resolver": {
+        typescript: {},
+      },
     },
   },
-)
+];
