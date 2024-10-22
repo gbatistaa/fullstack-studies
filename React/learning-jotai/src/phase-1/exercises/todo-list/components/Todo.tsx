3d48a0cb8f838d389eed3c1e09b9aeab827437cb
gbatistaa <gabrielbatistabarbosa4@gmail.com>
2024-10-22 13:56:38 -0300

    creation of the list container and the create item function

--- React/learning-jotai/src/phase-1/exercises/todo-list/components/Todo.tsx
+++ React/learning-jotai/src/phase-1/exercises/todo-list/components/Todo.tsx
@@ -3,16 +3,24 @@ import ListItem from "../classes/listItem";
 import styles from "../css/todo.module.css";
 import Item from "./Item";
 
-const initialValue: ListItem[] = [{ itemText: "test", status: true }];
-export const listAtom = atom<ListItem[]>(initialValue);
+export const listAtom = atom<ListItem[]>([new ListItem()]);
 
 function Todo(): JSX.Element {
   const [list, setList] = useAtom(listAtom);
+  const createItem = (): ListItem => {
+    const item = new ListItem();
+    setList((prevList) => prevList.concat([item]));
+    return item;
+  };
+
   return (
     <div className={styles.listContainer}>
-      {list.map((item, index) => {
-        return <Item info={item} key={index} />;
-      })}
+      <h1>TODO LIST</h1>
+      <div className={styles.list}>
+        {list.map((item, index) => {
+          return <Item info={item} key={index} />;
+        })}
+      </div>
     </div>
   );
 }
