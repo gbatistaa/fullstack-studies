bc0d33386a5aba081cf1a3c645ac8a3e79cec213
gbatistaa <gabrielbatistabarbosa4@gmail.com>
2024-10-22 10:36:25 -0300

    putting on new folder

---
+++ React/learning-jotai/src/phase-1/exercises/todo-list/classes/listItem.tsx
@@ -0,0 +1,12 @@
+type Status = "completed" | "in progress" | "not done";
+class ListItem {
+  public itemText: string;
+  public status: Status;
+
+  constructor(itemText: string, status: Status) {
+    this.itemText = itemText;
+    this.status = status;
+  }
+}
+
+export default ListItem;
