8c2d22955089ae46156a27a464efa266103028d2
gbatistaa <gabrielbatistabarbosa4@gmail.com>
2024-10-22 11:17:53 -0300

    created the todo list component structure and typing

--- React/learning-jotai/src/phase-1/exercises/todo-list/components/Todo.tsx
+++ React/learning-jotai/src/phase-1/exercises/todo-list/components/Todo.tsx
@@ -1,10 +1,20 @@
 import { atom, useAtom } from "jotai";
-import ListItem from "./listItem";
-import styles from "./todo.module.css";
+import ListItem from "../classes/listItem";
+import styles from "../css/todo.module.css";
+import Item from "./Item";
 
-export const listAtom: ListItem[] = atom();
+const initialValue: ListItem[] = [{ itemText: "test", status: true }];
+export const listAtom = atom<ListItem[]>(initialValue);
 
-function Todo() {
+function Todo(): JSX.Element {
   const [list, setList] = useAtom(listAtom);
-  return <div className={styles.listContainer}></div>;
+  return (
+    <div className={styles.listContainer}>
+      {list.map((item, index) => {
+        return <Item info={item} key={index} />;
+      })}
+    </div>
+  );
 }
+
+export default Todo;
