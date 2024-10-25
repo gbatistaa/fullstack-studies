20667db0a429128d03b39760521d8a82a8de2622
gbatistaa <gabrielbatistabarbosa4@gmail.com>
2024-10-25 08:43:15 -0300

    basic structure of and derivated atom

--- React/learning-jotai/src/phase-2/index.tsx
+++ React/learning-jotai/src/phase-2/index.tsx
@@ -4,10 +4,26 @@
   - Atualizações dependentes e memorização de estados derivados
 */
 
+import { atom, useAtom } from "jotai";
+import React from "react";
+
+const countAtom = atom(0);
+const derivativeAtom = atom((get) => get(countAtom) * 2);
+
 function Index(): JSX.Element {
+  const [count, setCount] = useAtom(countAtom);
+  const [derivative] = useAtom(derivativeAtom);
+
+  const handleClick = (event: React.MouseEvent): void => {
+    event.preventDefault();
+    setCount((countValue) => countValue + 1);
+  };
+
   return (
     <div>
-      <h1>Hello World!</h1>
+      <h2>Primary value: {count}</h2>
+      <h2>Derivative value: {derivative}</h2>
+      <button onClick={(e) => handleClick(e)}>Incrementor</button>
     </div>
   );
 }
