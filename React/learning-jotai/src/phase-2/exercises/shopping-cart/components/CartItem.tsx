529d2c30937994db52c777b35b1d6ff7ed3621d1
gbatistaa <gabrielbatistabarbosa4@gmail.com>
2024-10-29 15:35:15 -0300

    otimization of the cart item component structure

--- React/learning-jotai/src/phase-2/exercises/shopping-cart/components/CartItem.tsx
+++ React/learning-jotai/src/phase-2/exercises/shopping-cart/components/CartItem.tsx
@@ -6,18 +6,9 @@ function CartItem({ index }: { index: number }): JSX.Element {
   const [cartList, setCartList] = useAtom(cartAtom);
   return (
     <section className={styles.itemContainer}>
-      <div className={styles.itemPropDiv}>
-        <input type="text" />
-        <p>{cartList[index].name}</p>
-      </div>
-      <div className={styles.itemPropDiv}>
-        <input type="text" />
-        <p>{cartList[index].price}</p>
-      </div>
-      <div className={styles.itemPropDiv}>
-        <input type="text" />
-        <p>{cartList[index].quantity}</p>
-      </div>
+      <h2>{cartList[index].name}</h2>
+      <h4>Price: {cartList[index].price}$</h4>
+      <h4>Quantity: {cartList[index].quantity}</h4>
     </section>
   );
 }
