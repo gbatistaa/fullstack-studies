ebe6094e6c144f4ba6fa73d246b68e41a2b7e4e6
gbatistaa <gabrielbatistabarbosa4@gmail.com>
2024-10-29 20:20:37 -0300

    importing and adding stylesheet on the cart container

--- React/learning-jotai/src/phase-2/exercises/shopping-cart/components/Cart.tsx
+++ React/learning-jotai/src/phase-2/exercises/shopping-cart/components/Cart.tsx
@@ -1,22 +1,22 @@
 import { atom, useAtom } from "jotai";
 import Item from "../classes/Item";
+import styles from "../css/cart.module.css";
 import CartItem from "./CartItem";
 import NewItemForm from "./NewItemForm";
 
-export const cartAtom = atom<Item[]>([
-  new Item("Apple", 1.5, 10),
-  new Item("Pinapple", 2.3, 15),
-]);
+export const cartAtom = atom<Item[]>([]);
 
 function Cart(): JSX.Element {
   const [shoppingCart, setShoppingCart] = useAtom(cartAtom);
   return (
-    <div>
+    <>
       <NewItemForm />
-      {shoppingCart.map((item, index) => {
-        return <CartItem index={index} key={index} />;
-      })}
-    </div>
+      <div className={styles.cartContainer}>
+        {shoppingCart.map((item, index) => {
+          return <CartItem index={index} key={index} />;
+        })}
+      </div>
+    </>
   );
 }
 
