770a22e302906511baf552ca3bc6a7ef49b2ddb3
gbatistaa <gabrielbatistabarbosa4@gmail.com>
2024-10-29 15:35:59 -0300

    adding of inital values and the new item creator form

--- React/learning-jotai/src/phase-2/exercises/shopping-cart/components/Cart.tsx
+++ React/learning-jotai/src/phase-2/exercises/shopping-cart/components/Cart.tsx
@@ -1,13 +1,18 @@
 import { atom, useAtom } from "jotai";
 import Item from "../classes/Item";
 import CartItem from "./CartItem";
+import NewItemForm from "./NewItemForm";
 
-export const cartAtom = atom<Item[]>([new Item()]);
+export const cartAtom = atom<Item[]>([
+  new Item("Apple", 1.5, 10),
+  new Item("Pinapple", 2.3, 15),
+]);
 
 function Cart(): JSX.Element {
   const [shoppingCart, setShoppingCart] = useAtom(cartAtom);
   return (
     <div>
+      <NewItemForm />
       {shoppingCart.map((item, index) => {
         return <CartItem index={index} key={index} />;
       })}
