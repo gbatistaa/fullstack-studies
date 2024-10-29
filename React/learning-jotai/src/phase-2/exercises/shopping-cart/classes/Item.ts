f346e129ede6dae389a5adb2b9d7b3af74e2acc4
gbatistaa <gabrielbatistabarbosa4@gmail.com>
2024-10-29 15:23:50 -0300

    changing the Item class to support default values and passed values

--- React/learning-jotai/src/phase-2/exercises/shopping-cart/classes/Item.ts
+++ React/learning-jotai/src/phase-2/exercises/shopping-cart/classes/Item.ts
@@ -1,12 +1,12 @@
 class Item {
-  public name: string = "";
-  public price: number = 0;
-  public quantity: number = 0;
+  public name: string;
+  public price: number;
+  public quantity: number;
 
-  constructor(name: string, price: number, quantity: number) {
-    this.name = name;
-    this.price = price;
-    this.quantity = quantity;
+  constructor(name?: string, price?: number, quantity?: number) {
+    this.name = name || "Unnamed";
+    this.price = price || 0;
+    this.quantity = quantity || 0;
   }
 }
 
