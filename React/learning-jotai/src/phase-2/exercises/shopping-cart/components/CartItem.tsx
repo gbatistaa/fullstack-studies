1306ce6f810789c331b5488499d0c046f0e29c4d
gbatistaa <gabrielbatistabarbosa4@gmail.com>
2024-10-29 11:18:31 -0300

    basic structure of the shopping cart item

---
+++ React/learning-jotai/src/phase-2/exercises/shopping-cart/components/CartItem.tsx
@@ -0,0 +1,11 @@
+function CartItem({ index }: { index: number }): JSX.Element {
+  return (
+    <div>
+      <p>Name</p>
+      <p>Price</p>
+      <p>Quantity</p>
+    </div>
+  );
+}
+
+export default CartItem;
