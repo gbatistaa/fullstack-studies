308df2590ead200426538ab0e9831d79731986d1
gbatistaa <gabrielbatistabarbosa4@gmail.com>
2024-11-04 16:42:57 -0300

    adding id prop

--- React/learning-jotai/src/phase-3/exercises/todo-list/classes/Todo.ts
+++ React/learning-jotai/src/phase-3/exercises/todo-list/classes/Todo.ts
@@ -2,11 +2,18 @@ class Todo {
   public name;
   public category;
   public isCompleted;
+  public id: number;
 
-  constructor(name?: string, category?: string, isCompleted?: boolean) {
+  constructor(
+    id: number,
+    name?: string,
+    category?: string,
+    isCompleted?: boolean,
+  ) {
     this.name = name ? name : "";
     this.category = category ? category : "";
     this.isCompleted = isCompleted ? isCompleted : false;
+    this.id = id;
   }
 }
 
