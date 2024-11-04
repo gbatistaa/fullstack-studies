6b3f6b5bc7962e0a25c444edbfa2213f65ea00f9
gbatistaa <gabrielbatistabarbosa4@gmail.com>
2024-11-04 17:25:00 -0300

    adding the list rendering  logic to the todo list using atom family

--- React/learning-jotai/src/phase-3/exercises/todo-list/components/TodoList.tsx
+++ React/learning-jotai/src/phase-3/exercises/todo-list/components/TodoList.tsx
@@ -1,14 +1,29 @@
-import { atom } from "jotai";
+import { atom, useAtom } from "jotai";
 import { atomFamily } from "jotai/utils";
+import Todos from "../classes/Todo";
+import Todo from "./Todo";
 
-const todoAtom = atom("");
-const todoListFamily = atomFamily((id) => atom());
+export const todoListFamily = atomFamily((id: number) =>
+  atom<Todos>(new Todos(id)),
+);
+
+export const todoIdsAtom = atom<number[]>([]);
 
 function TodoList(): JSX.Element {
+  const [ids, setIds] = useAtom(todoIdsAtom);
+
+  const createNewTodo = (id: number): void => {
+    console.log(todoListFamily(id).init);
+  };
+
   return (
-    <div>
-      <h1>Hello World!</h1>
-    </div>
+    <section>
+      <div>
+        {ids.map((id: number) => {
+          return <Todo key={id} id={id} />;
+        })}
+      </div>
+    </section>
   );
 }
 
