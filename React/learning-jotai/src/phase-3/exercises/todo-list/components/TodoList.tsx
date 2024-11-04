29eb097a8df18f11e18e474e8e4b4a801a65e7ef
gbatistaa <gabrielbatistabarbosa4@gmail.com>
2024-11-04 17:55:34 -0300

    adding the new todo form and adding class names on html elements

--- React/learning-jotai/src/phase-3/exercises/todo-list/components/TodoList.tsx
+++ React/learning-jotai/src/phase-3/exercises/todo-list/components/TodoList.tsx
@@ -1,28 +1,36 @@
 import { atom, useAtom } from "jotai";
 import { atomFamily } from "jotai/utils";
 import Todos from "../classes/Todo";
+import styles from "../css/todoList.module.css";
 import Todo from "./Todo";
 
 export const todoListFamily = atomFamily((id: number) =>
   atom<Todos>(new Todos(id)),
 );
-
 export const todoIdsAtom = atom<number[]>([]);
 
 function TodoList(): JSX.Element {
   const [ids, setIds] = useAtom(todoIdsAtom);
 
   const createNewTodo = (id: number): void => {
-    console.log(todoListFamily(id).init);
+    todoListFamily(id);
   };
 
   return (
-    <section>
-      <div>
+    <section className={styles.todoListContainer}>
+      <div className={styles.todoList}>
         {ids.map((id: number) => {
           return <Todo key={id} id={id} />;
         })}
       </div>
+      <form className={styles.createTodoForm}>
+        <div className={styles.newTodoInputsDiv}>
+          <input type="text" />
+          <input type="text" />
+          <input type="checkbox" name="" id="" />
+        </div>
+        <button type="submit">Create Todo</button>
+      </form>
     </section>
   );
 }
