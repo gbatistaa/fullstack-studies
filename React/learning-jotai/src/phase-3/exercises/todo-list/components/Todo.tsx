5c8a7a778063a6a3c68f682dc32d3356e979c3f8
gbatistaa <gabrielbatistabarbosa4@gmail.com>
2024-11-04 17:41:40 -0300

    using the atomFamily to connect each todo with the todo list

--- React/learning-jotai/src/phase-3/exercises/todo-list/components/Todo.tsx
+++ React/learning-jotai/src/phase-3/exercises/todo-list/components/Todo.tsx
@@ -1,7 +1,13 @@
-function Todo(): JSX.Element {
+import { useAtom } from "jotai";
+import { todoListFamily } from "./TodoList";
+
+function Todo({ id }: { id: number }): JSX.Element {
+  const [todo, setTodo] = useAtom(todoListFamily(id));
   return (
     <div>
-      <h1></h1>
+      <h1>{todo.name}</h1>
+      <h1>{todo.category}</h1>
+      <h1>{todo.isCompleted ? "yes" : "no"}</h1>
     </div>
   );
 }
