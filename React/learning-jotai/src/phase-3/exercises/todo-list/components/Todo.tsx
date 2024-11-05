ef380451a6bed118a3da62b5d34b5b6fd3737fa6
gbatistaa <gabrielbatistabarbosa4@gmail.com>
2024-11-05 15:06:53 -0300

    added the deleting functionality and button on each todo

--- React/learning-jotai/src/phase-3/exercises/todo-list/components/Todo.tsx
+++ React/learning-jotai/src/phase-3/exercises/todo-list/components/Todo.tsx
@@ -1,13 +1,25 @@
 import { useAtom } from "jotai";
-import { todoListFamily } from "./TodoList";
+import React from "react";
+import styles from "../css/todo.module.css";
+import { todoIdsAtom, todoListFamily } from "./TodoList";
 
 function Todo({ id }: { id: number }): JSX.Element {
-  const [todo, setTodo] = useAtom(todoListFamily(id));
+  const [todo] = useAtom(todoListFamily(id)); // Remova os parênteses adicionais
+  const [ids, setIds] = useAtom(todoIdsAtom);
+
+  const deleteTodo = (event: React.MouseEvent<HTMLButtonElement>): void => {
+    event.preventDefault();
+    setIds(ids.filter((existingId) => existingId !== id));
+  };
+
   return (
-    <div>
+    <div className={styles.todoInfo}>
       <h1>{todo.name}</h1>
       <h1>{todo.category}</h1>
       <h1>{todo.isCompleted ? "yes" : "no"}</h1>
+      <button type="button" onClick={deleteTodo}>
+        X
+      </button>
     </div>
   );
 }
