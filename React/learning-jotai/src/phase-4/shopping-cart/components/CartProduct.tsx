277e257a90ce1ddac5bb48d546a89477b08ae639
gbatistaa <gabrielbatistabarbosa4@gmail.com>
2024-11-10 11:22:55 -0300

    basic structure of a cart product component

--- React/learning-jotai/src/phase-4/shopping-cart/components/CartProduct.tsx
+++ React/learning-jotai/src/phase-4/shopping-cart/components/CartProduct.tsx
@@ -1,8 +1,19 @@
+import { useAtom } from "jotai";
+import { cartAtomStorage } from "./ShoppingCart";
+
 function CartProduct({ id }: { id: number }): JSX.Element {
+  const [products] = useAtom(cartAtomStorage);
+  const product = products.find((prod) => prod.id === id);
+
+  if (!product) return <div>Produto Não Encontrado.</div>;
+
   return (
-    <>
-      <h1>{id}</h1>
-    </>
+    <section>
+      <div>{product.id}</div>
+      <div>{product.name}</div>
+      <div>{product.quantity}</div>
+      <div>{product.price}</div>
+    </section>
   );
 }
 export default CartProduct;
