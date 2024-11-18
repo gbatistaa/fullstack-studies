371c87f88e7618f543bde281791df095a0f2e650
gbatistaa <gabrielbatistabarbosa4@gmail.com>
2024-11-18 11:32:28 -0300

    product filter main component

---
+++ React/learning-jotai/src/phase-5/exercises/product-filter/components/ProductFilter.tsx
@@ -0,0 +1,11 @@
+import CategoryFilter from "./CategoryFilter";
+
+function ProductFilter(): JSX.Element {
+  return (
+    <>
+      <CategoryFilter />
+    </>
+  );
+}
+
+export default ProductFilter;
