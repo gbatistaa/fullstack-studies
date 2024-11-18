9697560ea8f3636b1e796490c20667ad9ae54560
gbatistaa <gabrielbatistabarbosa4@gmail.com>
2024-11-18 11:21:48 -0300

    removing the reference usage

--- React/learning-jotai/src/phase-5/exercises/product-filter/ProductFilter.tsx
+++ React/learning-jotai/src/phase-5/exercises/product-filter/ProductFilter.tsx
@@ -1,58 +1,52 @@
 import { useAtom } from "jotai";
 import { atomWithHash } from "jotai-location";
 import { atomWithStorage } from "jotai/utils";
-import React, { useRef } from "react";
+import React from "react";
 import { Product } from "./Product";
 import styles from "./productFilter.module.css";
 
 export type Category = "eletronics&" | "clothes&" | "toys&" | "";
 
-const categoryAtom = atomWithHash<Category[]>("category", [""]);
+const categoryAtom = atomWithHash<Category[]>("category", []);
 const priceRangeAtom = atomWithHash<number[]>("price-range", [0, 100]);
 const productsAtom = atomWithStorage<Product[]>("products", []);
 
 function ProductFilter(): JSX.Element {
-  const categoryRef = useRef<Category[]>([]);
   const [category, setCategory] = useAtom(categoryAtom);
   const [priceRange, setPriceRange] = useAtom(priceRangeAtom);
 
   const handleCheckbox = (event: React.ChangeEvent<HTMLInputElement>): void => {
     const { checked, name } = event.target;
+    event.target.checked = !checked;
     if (checked) {
-      categoryRef.current = [...categoryRef.current, name as Category];
+      setCategory((prevCats) => prevCats.concat([name as Category]));
     } else {
-      categoryRef.current = [
-        ...categoryRef.current.filter((cat) => cat != (name as Category)),
-      ];
+      setCategory((prevCats) =>
+        prevCats.filter((cat) => cat !== (name as Category)),
+      );
     }
-    console.log(categoryRef.current);
-  };
-
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
-    event.preventDefault();
-    setCategory(categoryRef.current);
   };
 
   return (
     <section className={styles.productFilterContainer}>
-      <form onSubmit={(e) => handleSubmit(e)}>
-        <input
-          type="checkbox"
-          name="eletronics&"
-          onChange={(e) => handleCheckbox(e)}
-        />
-        <input
-          type="checkbox"
-          name="clothes&"
-          onChange={(e) => handleCheckbox(e)}
-        />
-        <input
-          type="checkbox"
-          name="toys&"
-          onChange={(e) => handleCheckbox(e)}
-        />
-        <button type="submit">Change Filter</button>
-      </form>
+      <input
+        type="checkbox"
+        name="eletronics&"
+        onChange={(e) => handleCheckbox(e)}
+        checked={category.includes("eletronics&")}
+      />
+      <input
+        type="checkbox"
+        name="clothes&"
+        onChange={(e) => handleCheckbox(e)}
+        checked={category.includes("clothes&")}
+      />
+      <input
+        type="checkbox"
+        name="toys&"
+        onChange={(e) => handleCheckbox(e)}
+        checked={category.includes("toys&")}
+      />
     </section>
   );
 }
