2b05137f8f2af99fdb9a6783fba9f7e59516df72
gbatistaa <gabrielbatistabarbosa4@gmail.com>
2024-11-18 10:15:43 -0300

    adding function to update the category list reference when checked

--- React/learning-jotai/src/phase-5/exercises/product-filter/ProductFilter.tsx
+++ React/learning-jotai/src/phase-5/exercises/product-filter/ProductFilter.tsx
@@ -1,28 +1,49 @@
 import { useAtom } from "jotai";
 import { atomWithHash } from "jotai-location";
 import { atomWithStorage } from "jotai/utils";
-import { useRef } from "react";
+import React, { useRef } from "react";
 import { Product } from "./Product";
 import styles from "./productFilter.module.css";
 
-export type Category = "eletronics" | "clothes" | "toys" | "all";
+export type Category = "eletronics" | "clothes" | "toys" | "";
 
-const categoryAtom = atomWithHash<Category>("category", "all");
+const categoryAtom = atomWithHash<Category>("category", "");
 const priceRangeAtom = atomWithHash<number[]>("price-range", [0, 100]);
 const productsAtom = atomWithStorage<Product[]>("products", []);
 
 function ProductFilter(): JSX.Element {
-  const categoryRef = useRef<Category>("all");
+  const categoryRef = useRef<Category[]>([]);
   const [category, setCategory] = useAtom(categoryAtom);
   const [priceRange, setPriceRange] = useAtom(priceRangeAtom);
+
+  const handleCheckbox = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    event.preventDefault();
+    const { checked, name } = event.target;
+    categoryRef.current = [
+      ...categoryRef.current,
+      checked ? (name as Category) : "",
+    ];
+  };
 
   return (
     <section className={styles.productFilterContainer}>
       <form>
-        <input type="radio" name="category" />
-        <input type="radio" name="category" />
-        <input type="radio" name="category" />
-        <input type="radio" name="category" />
+        <input
+          type="checkbox"
+          name="eletronics"
+          onChange={(e) => handleCheckbox(e)}
+        />
+        <input
+          type="checkbox"
+          name="clothes"
+          onChange={(e) => handleCheckbox(e)}
+        />
+        <input
+          type="checkbox"
+          name="toys"
+          onChange={(e) => handleCheckbox(e)}
+        />
+        <button type="submit">Change Filter</button>
       </form>
     </section>
   );
