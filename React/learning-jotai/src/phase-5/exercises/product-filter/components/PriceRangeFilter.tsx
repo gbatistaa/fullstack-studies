32861bc9969e28255e2449e02d7d81d38aa90181
gbatistaa <gabrielbatistabarbosa4@gmail.com>
2024-11-18 12:34:33 -0300

    handling changes on the price range inputs

--- React/learning-jotai/src/phase-5/exercises/product-filter/components/PriceRangeFilter.tsx
+++ React/learning-jotai/src/phase-5/exercises/product-filter/components/PriceRangeFilter.tsx
@@ -1,4 +1,6 @@
+import { useAtom } from "jotai";
 import { atomWithHash } from "jotai-location";
+import React from "react";
 
 const priceRangeAtom = atomWithHash("price-range", [0, 100], {
   serialize: (val) => `${val[0]}-${val[1]}`,
@@ -6,11 +8,35 @@ const priceRangeAtom = atomWithHash("price-range", [0, 100], {
 });
 
 function PriceRangeFilter(): JSX.Element {
+  const [priceRange, setPriceRange] = useAtom(priceRangeAtom);
+
+  const handlePriceChange = (
+    event: React.ChangeEvent<HTMLInputElement>,
+  ): void => {
+    event.preventDefault();
+    const { name, value } = event.target;
+    setPriceRange((prevRange) =>
+      name === "price-min"
+        ? [Number(value), prevRange[1]]
+        : [prevRange[0], Number(value)],
+    );
+  };
+
   return (
     <div>
-      <input type="number" name="price-min" placeholder="Min. price" />
+      <input
+        type="number"
+        name="price-min"
+        placeholder="Min. price"
+        onChange={(e) => handlePriceChange(e)}
+      />
       <br />
-      <input type="number" name="price-max" placeholder="Max. price" />
+      <input
+        type="number"
+        name="price-max"
+        placeholder="Max. price"
+        onChange={(e) => handlePriceChange(e)}
+      />
     </div>
   );
 }
