78106c5c19dd35b5acde3ccb123247664ca540f7
gbatistaa <gabrielbatistabarbosa4@gmail.com>
2024-11-24 13:10:44 -0300

    class 1: learing fonts and colors with Tailwind

---
+++ Tailwind/src/components/1-fonts-and-colors.tsx
@@ -0,0 +1,19 @@
+function FontsAndColors(): JSX.Element {
+  return (
+    <div className='bg-gray-100 h-screen w-screen'>
+      <h2 className='text-sm text-gray-500'>Hello World!</h2>
+      <h2 className='text-base text-red-800'>Hello World!</h2>
+      <h2 className='text-lg text-yellow-400'>Hello World!</h2>
+      <h2 className='text-xl tracking-widest'>Hello World!</h2>
+      <h2 className='text-2xl font-bold'>Hello World!</h2>
+      <h2 className='text-3xl font-black tracking-widest'>Hello World!</h2>
+      <p className='text-center'>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores,
+        iste vitae at dicta et ducimus quibusdam cupiditate repellendus corporis
+        dolorum magni rerum beatae. Illo odio facilis cum ea facere labore.
+      </p>
+    </div>
+  )
+}
+
+export default FontsAndColors
