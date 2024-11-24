0a5425a79ed41df109d4a415c23bc242616ca6ca
gbatistaa <gabrielbatistabarbosa4@gmail.com>
2024-11-24 13:10:11 -0300

    adding start files

---
+++ Tailwind/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import FontsAndColors from './components/1-fonts-and-colors'
+
+function App() {
+
+  // Class-1: Fonts and colors
+
+  return (
+    <FontsAndColors />
+  )
+}
+
+export default App
