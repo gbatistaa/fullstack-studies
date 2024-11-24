50b60d9b7fa86fc271f0f8f365917972c4672636
gbatistaa <gabrielbatistabarbosa4@gmail.com>
2024-11-24 17:11:36 -0300

    pushing the laering background with tailwind css file

---
+++ Tailwind/src/components/4-background.tsx
@@ -0,0 +1,14 @@
+function Background(): JSX.Element {
+  return (
+    <div>
+      <div className="m-5 p-4 bg-blue-900 text-white bg-gradient-to-bl from-yellow-400 via-red-500 to-pink-500">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Non esse doloribus ducimus aliquam quos. Dignissimos modi adipisci porro, obcaecati illum quaerat ex? Amet perferendis recusandae reiciendis enim ullam unde laborum!
+      </div>
+      <div className="m-5 p-4 text-white bg-gradient-to-r from-gray-900 via-blue-500 to-blue-200">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Non esse doloribus ducimus aliquam quos. Dignissimos modi adipisci porro, obcaecati illum quaerat ex? Amet perferendis recusandae reiciendis enim ullam unde laborum!
+      </div>
+    </div>
+  )
+}
+
+export default Background;
