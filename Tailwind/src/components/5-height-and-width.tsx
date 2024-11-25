07d6c06451841554017575768262ab94f345c0b5
gbatistaa <gabrielbatistabarbosa4@gmail.com>
2024-11-25 11:21:40 -0300

    learing height and width with tailwind css

---
+++ Tailwind/src/components/5-height-and-width.tsx
@@ -0,0 +1,11 @@
+function HeightAndWidth(): JSX.Element {
+  return (
+    <div className="p-5 bg-blue-400 bg-gradient-to-r from-orange-400 via-red-400 max-w-xl h-fit">
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maxime labore alias perferendis?
+      Error deleniti, quod eligendi quia incidunt corrupti molestias exercitationem excepturi
+      assumenda placeat perspiciatis laudantium repellat illum architecto fuga!
+    </div>
+  )
+}
+
+export default HeightAndWidth;
