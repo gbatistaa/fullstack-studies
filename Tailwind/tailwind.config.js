a1d4a5b0e20788b7fe4e4e86bd015d849eed5570
gbatistaa <gabrielbatistabarbosa4@gmail.com>
2024-11-26 12:09:20 -0300

    adding dark mode config and personalized colors

--- Tailwind/tailwind.config.js
+++ Tailwind/tailwind.config.js
@@ -1,9 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   mode: "jit",
+  darkMode: "media",
   content: ["./*.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        base: {
+          100: "#26daeb",
+          200: "#22c5d4",
+          300: "#1da8b5",
+          400: "#15858f",
+        },
+      },
+    },
   },
   plugins: [],
 };
