82055701e25480f6e9e0c1d4c4b2f72676aca44f
gbatistaa <gabrielbatistabarbosa4@gmail.com>
2024-11-26 12:40:48 -0300

    learnig responsivity with tailwind css

---
+++ Tailwind/src/components/8-responsivity.tsx
@@ -0,0 +1,9 @@
+function Responsivity(): JSX.Element {
+  return (
+    <div className="h-fit w-screen bg-base flex justify-center items-center">
+      <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl">Hello TailwindCSS</h1>
+    </div>
+  )
+}
+
+export default Responsivity;
