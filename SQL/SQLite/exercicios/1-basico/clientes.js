5831121844c14f8a05b83d104c4545f654a1a59a
gbatistaa <gbatistadev@gmail.com>
2025-05-28 20:52:11 +0000

    chore: remove useless error treatment

--- SQL/SQLite/exercicios/1-basico/clientes.js
+++ SQL/SQLite/exercicios/1-basico/clientes.js
@@ -1,15 +1,5 @@
-import { handleSQLiteError } from "../../functions/erros.js";
 import { criarTabelas } from "./sql/criarTabela.js";
 import { inserirClientes } from "./sql/inserirClientes.js";
 
-async function main() {
-  try {
-    await criarTabelas();
-
-    await inserirClientes();
-  } catch (error) {
-    handleSQLiteError(error);
-  }
-}
-
-main();
+await criarTabelas();
+await inserirClientes();
