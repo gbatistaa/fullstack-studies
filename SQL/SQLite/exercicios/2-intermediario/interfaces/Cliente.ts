56e1434fda1a10e5a477bc86da69fb3472d64e58
Gabriel Barbosa <gbatistadev@gmail.com>
2025-06-05 10:06:21 -0300

    interfaces de entidades criadas

---
+++ SQL/SQLite/exercicios/2-intermediario/interfaces/Cliente.ts
@@ -0,0 +1,6 @@
+export default interface Cliente {
+  id: number;
+  nome: string;
+  email: string;
+  dataCadastro: Date;
+}
---
+++ SQL/SQLite/exercicios/2-intermediario/interfaces/Pedido.ts
@@ -0,0 +1,6 @@
+export default interface Pedido {
+  id: number;
+  clientId: number;
+  valorTotal: number;
+  dataPedido: Date;
+}
