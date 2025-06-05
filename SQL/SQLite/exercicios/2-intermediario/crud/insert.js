a79e98dfc76426a13146a958243896871353e0d8
Gabriel Barbosa <gbatistadev@gmail.com>
2025-06-04 22:48:55 -0300

    null

--- SQL/SQLite/exercicios/2-intermediario/crud/insert.js
+++ SQL/SQLite/exercicios/2-intermediario/crud/insert.js
@@ -2,10 +2,11 @@ import Cliente from "../models/clientes.js";
 
 const cadastrarCliente = async () => {
   try {
-    const resultadoCadastro = Cliente.create({
-      
-    })
+    const resultadoCadastro = Cliente.create({});
+    console.log(resultadoCadastro);
   } catch (error) {
     console.log(error);
   }
-}
+};
+
+export default cadastrarCliente;
