66d9b12921e5a4bd61e601ba48238c81f642eb61
Gabriel Barbosa <gbatistadev@gmail.com>
2025-06-05 09:43:03 -0300

    terminando de construir o model de pedidos

--- SQL/SQLite/exercicios/2-intermediario/models/pedidos.js
+++ SQL/SQLite/exercicios/2-intermediario/models/pedidos.js
@@ -5,8 +5,24 @@ const Pedido = connection.define("Pedido", {
   id: {
     type: DataTypes.INTEGER,
     autoIncrement: true,
-    primaryKey: true
-  }
-})
+    primaryKey: true,
+  },
+  client_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: "Cliente",
+      key: "id",
+    },
+  },
+  valor_total: {
+    type: DataTypes.FLOAT,
+    allowNull: false,
+  },
+  data_pedido: {
+    type: DataTypes.DATE,
+    allowNull: false,
+  },
+});
 
 export default Pedido;
