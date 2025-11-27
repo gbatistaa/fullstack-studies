c099ae942306927ec9f4dee0bad10e2a3150d07b
gbatistaa <gbatistadev@gmail.com>
2025-11-27 07:42:32 -0300

    feat: adicionar animação de movimento do jogador e lógica de controle

--- Unity/my-project/Assets/Scripts/3-Animation/Player.cs
+++ Unity/my-project/Assets/Scripts/3-Animation/Player.cs
@@ -1,16 +1,37 @@
+using System;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
 public class Player3 : MonoBehaviour
 {
   private Rigidbody2D rb;
+  private Animator anim;
 
   [SerializeField]
-  private float moveSpeed = 10.0f;
+  private float moveSpeed = 11f;
 
   [SerializeField]
-  private float jumpForce = 8f;
+  private float jumpForce = 10.0f;
   private float xInput;
+
+  [SerializeField]
+  private bool facingRight = true;
+
+  private void Awake()
+  {
+    // Use the GetComponent function to be able to modify a private component on the Unity Editor:
+    rb = GetComponent<Rigidbody2D>();
+    // GetComponentInChildren because the Animator component is a child of Player
+    anim = GetComponentInChildren<Animator>();
+  }
+
+  private void Update()
+  {
+    HandleJump();
+    HandleMovement();
+    HandleAnimator();
+    HandleFlip();
+  }
 
   // Jump functionality
   private void Jump()
@@ -18,27 +39,34 @@ public class Player3 : MonoBehaviour
     rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
   }
 
-  public void HandleMovement()
+  private void HandleMovement()
   {
+    xInput = Input.GetAxisRaw("Horizontal");
     rb.linearVelocity = new Vector2(xInput * moveSpeed, rb.linearVelocity.y);
   }
 
-  public void HandleInput()
+  private void HandleAnimator()
+  {
+    bool isMoving = Mathf.Abs(xInput) > 0.1f;
+    anim.SetBool("isMoving", isMoving);
+  }
+
+  private void HandleJump()
   {
     if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow))
       Jump();
   }
 
-  private void Awake()
+  private void HandleFlip()
   {
-    // Use the GetComponent function to be able to modify a private component on the Unity Editor:
-    rb = GetComponent<Rigidbody2D>();
+    if (rb.linearVelocity.x > 0 && !facingRight || rb.linearVelocity.x < 0 && facingRight)
+      Flip();
   }
 
-  private void Update()
+  private void Flip()
   {
-    xInput = Input.GetAxisRaw("Horizontal");
-    HandleInput();
-    HandleMovement();
+    // Makes that the caracther flips when change the size
+    transform.Rotate(0, 180, 0);
+    facingRight = !facingRight;
   }
 }
